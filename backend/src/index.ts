4ef37ae31478572567163e950097a8ddf4931b37
pablo.raviola <pablo.raviola@gmail.com>
2021-08-30 23:31:00 -0300

    db-config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import * as dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import * as dotenv from "dotenv";
 
 const app = express();
 
+mongoose
+  .connect(`${process.env.DATABASE_URL}`)
+  .then(() => console.log("MongoDB Connected.."))
+  .catch((err) => console.log(err));
+
 dotenv.config();
 app.use(cors());
-app.use(bodyParser.json({ limit: '20mb' }));
-app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
-
+app.use(bodyParser.json({ limit: "20mb" }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
