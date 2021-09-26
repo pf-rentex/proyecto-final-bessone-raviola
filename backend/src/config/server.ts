032f65d96243fdd918bb704d0667fd843c3994a1
Gino Raviola <ginoraviola@gmail.com>
2021-09-26 18:44:36 -0300

    restructuring

--- backend/src/config/server.ts
+++ backend/src/config/server.ts
@@ -2,14 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import authMiddleware from '../middlewares/auth';
 
-import users from '../routes/api/users';
-import auth from '../routes/api/auth';
-import tenants from '../routes/api/tenants';
-import owners from '../routes/api/owners';
-import realEstates from '../routes/api/realEstates';
-
+import router from "../routes";
 
 const initializeServer = () => {
 
@@ -21,16 +15,13 @@ const initializeServer = () => {
     app.use(bodyParser.json({limit: '20mb'}));
     app.use(bodyParser.urlencoded({limit: '20mb', extended: true}));
 
-    app.use('/api/users', users);
-    app.use('/api/auth', auth);
-    app.use('/api/tenants', authMiddleware, tenants);
-    app.use('/api/owners', authMiddleware, owners);
-    app.use('/api/realEstates', authMiddleware, realEstates);
+    app.use(router);
 
     const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    const server = app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
+    return { app, server };
   } catch (e) {
     console.error(`Error launching server.. ${e}`);
   }
