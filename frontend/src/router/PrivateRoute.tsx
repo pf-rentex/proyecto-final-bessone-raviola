07c423fe8238234d5920f30d36f452291bf30980
Gino Raviola <ginoraviola@gmail.com>
2021-10-04 23:14:44 -0300

    add private routes

---
+++ frontend/src/router/PrivateRoute.tsx
@@ -0,0 +1,29 @@
+import React, {useEffect} from "react";
+import {connect} from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+import { IProfileData } from "../reducers/auth";
+
+interface IPrivateRouteProps {
+  exact?: boolean;
+  path: string;
+  component: () => JSX.Element;
+  profile: null | IProfileData;
+  isAuthenticated: boolean;
+}
+
+const PrivateRoute = ( props: IPrivateRouteProps ) => {
+  const { component, profile, isAuthenticated, ...rest } = props;
+
+
+  if (isAuthenticated) {
+    return <Route { ...rest } component={component} />
+  }
+  return <Redirect to='/'/>
+}
+
+const mapStateToProps = (state: any) => ({
+  profile: state.auth.profile,
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, {})(PrivateRoute);
