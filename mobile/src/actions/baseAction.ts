38c5efa296cab7ae71a4b461562786b4f698055d
Gino Raviola <ginoraviola@gmail.com>
2021-10-12 22:48:06 -0300

    remove unused shit

--- mobile/src/actions/baseAction.ts
+++
@@ -1,9 +0,0 @@
-import {GET, FETCH} from "./types";
-import {Dispatch} from "redux";
-
-export const triggerGetSalutation = () => (dispatch: Dispatch<any>) => {
-  dispatch({
-    type: GET,
-    payload: 'Hello World -> straight outta action -> replace with async API response.'
-  });
-}
