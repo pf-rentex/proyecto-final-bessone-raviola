e2b8e15937ec6215a8411caabdb60d237468c008
Pablo Raviola <pablo.raviola@externos-ar.cencosud.com>
2021-10-30 01:28:45 -0300

    attributes & measuerements tests

--- backend/src/tests/getPropertiesAssert.test.ts
+++ backend/src/tests/getPropertiesAssert.test.ts
@@ -20,4 +20,23 @@ describe('Get properties: ', () => {
             .query({ limit: 10 });
         assert.isAtMost(result.body.properties.length, 10);
     }).timeout(5000);
+    it('Check result attributes', async () => {
+        const result = await chai
+            .request(url)
+            .get('/api/properties')
+            .query({ attributes: { city: 'San Francisco' } });
+        result.body.properties.map((property) => {
+            assert.equal(property.city, 'San Francisco');
+        });
+    }).timeout(5000);
+    it('Check result measurements', async () => {
+        const result = await chai
+            .request(url)
+            .get('/api/properties')
+            .query({ measurements: { price: { $gt: 1000, $lt: 50000 } } });
+        result.body.properties.map((property) => {
+            assert.isAbove(property.price, 1000);
+            assert.isBelow(property.price, 50000);
+        });
+    }).timeout(5000);
 });
