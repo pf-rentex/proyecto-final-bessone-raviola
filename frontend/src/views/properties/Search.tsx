336a376d78c2b8a890b19676f082f6670d16ccb9
Gino Raviola <ginoraviola@Ginos-MacBook-Pro-2.local>
2022-05-15 18:13:01 -0300

    add datepicker + behaviour

--- frontend/src/views/properties/Search.tsx
+++ frontend/src/views/properties/Search.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import { connect } from 'react-redux';
 import { BiSearch } from 'react-icons/bi';
 import { getProperties } from '../../actions/properties';
 import { IProperty } from '../../reducers/properties';
 import Listing from '../../components/properties/Listing';
+import Visit from "../../components/properties/Visit";
 
 interface ISearchProps {
     getProperties: Function;
@@ -12,11 +13,18 @@ interface ISearchProps {
 }
 
 const Search = ({ getProperties, properties, isLoading }: ISearchProps) => {
+    const [showVisitDialog, setShowVisitDialog] = useState(false);
     useEffect(() => {
         getProperties();
     }, []);
+
+    const toggleVisitDialog = () => {
+        setShowVisitDialog(!showVisitDialog);
+    };
+
     return (
         <section className="flex flex-col h-full w-full bg-gradient-to-b from-bg-gradient-8 to-bg-gradient-9 px-5 lg:px-20 py-10">
+            {showVisitDialog && <Visit onClose={() => toggleVisitDialog()}/>}
             <div className="flex justify-center lg:justify-end">
                 <div className="relative w-full lg:w-9/12 text-gray-600 focus-within:text-gray-400">
                     <span className="absolute inset-y-0 left-0 flex items-center pl-2">
@@ -36,7 +44,7 @@ const Search = ({ getProperties, properties, isLoading }: ISearchProps) => {
                 ) : (
                     <div className="w-full lg:w-9/12">
                         {properties.map((property, index) => {
-                            return <Listing key={index} />;
+                            return <Listing key={index} onOpenVisitDialog={toggleVisitDialog} />;
                         })}
                     </div>
                 )}
