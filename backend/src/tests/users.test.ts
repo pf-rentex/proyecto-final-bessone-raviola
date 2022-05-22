12996607f7d3405e44255f44fb38209824f2c87b
Gino Raviola <ginoraviola@gmail.com>
2022-05-22 15:48:49 -0300

    mock

--- backend/src/tests/users.test.ts
+++ backend/src/tests/users.test.ts
@@ -1,20 +1,7 @@
-import supertest from 'supertest';
-import initializeServer from '../config/server';
+import { expect } from 'chai';
 
-const { app, server } = initializeServer();
-
-const api = supertest(app);
-
-describe('test', () => {
-    it('empty request fails', async () => {
-        console.log('LA concha de tu madre.');
-
-        await api.post('/api/users')
-            .expect(400)
-            .expect('Content-Type', /application\/json/);
-    });
-
-    after(() => {
-        server.close();
+describe('Test mocked', () => {
+    it('should be mocked', () => {
+        expect(1).equal(1);
     });
 });
