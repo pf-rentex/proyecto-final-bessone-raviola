d105e64ca5b0e3d0d491ea57ec0ce0197b5665ed
Gino Raviola <ginoraviola@gmail.com>
2022-05-31 22:41:25 -0300

    add line-clamp plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,11 +2,11 @@ const defaultTheme = require('./src/styles/index');
 
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: defaultTheme,
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: defaultTheme,
+    variants: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/line-clamp')],
+};
