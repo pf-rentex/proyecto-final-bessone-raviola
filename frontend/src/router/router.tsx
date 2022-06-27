36b0ad9f3671f99b499d8e836a87f2210b2ce3c5
Gino Raviola <ginoraviola@gmail.com>
2022-06-27 01:05:46 -0300

    declarative routing

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,23 +1,10 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Auth from '../views/auth/Auth';
-import Onboarding from '../views/auth/Onboarding';
 import PrivateRoute from './PrivateRoute';
-import Home from '../views/Home';
-import UserProfile from '../views/profile/UserProfile';
-import Search from '../views/properties/Search';
-import RequestForm from '../views/rent/request/RequestForm';
-import Properties from '../views/properties/Properties';
-import CreateProperty from '../views/properties/CreateProperty';
-import Publication from '../views/publications/Publication';
-import Claims from '../views/claims/Claims';
-import ClaimDetails from '../views/claims/ClaimDetails';
-import CreateClaim from '../views/claims/CreateClaim';
-import Contracts from '../views/contracts/Contracts';
-import RentRequests from '../views/rent/request/Requests';
-import ContractDetails from '../views/contracts/ContractDetails';
 import Header from '../components/commons/header/Header';
 import Sidebar from '../components/commons/Sidebar/Sidebar';
+
+import routes from './routes';
 
 export default function Router() {
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -27,21 +14,15 @@ export default function Router() {
             <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
             <Header setIsOpenSidebar={setIsOpen} />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Auth />} />
-                <Route path="/onboarding" element={<PrivateRoute component={<Onboarding />} />} />
-                <Route path="/profile" element={<PrivateRoute component={<UserProfile />} />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/rent/request" element={<RequestForm />} />
-                <Route path="/publication/:id" element={<Publication />} />
-                <Route path="/claims" element={<Claims />} />
-                <Route path="/claims/create" element={<CreateClaim />} />
-                <Route path="/claims/:id" element={<ClaimDetails />} />
-                <Route path="/contracts" element={<Contracts />} />
-                <Route path="/rent/requests" element={<RentRequests />} />
-                <Route path="/contracts/:id" element={<ContractDetails />} />
-                <Route path="properties" element={<PrivateRoute component={<Properties />} />} />
-                <Route path="/properties/create" element={<PrivateRoute component={<CreateProperty />} />} />
+                {routes.map((route, index) => {
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={route.isPrivate ? <PrivateRoute component={route.component} /> : route.component}
+                        />
+                    );
+                })}
             </Routes>
         </BrowserRouter>
     );
